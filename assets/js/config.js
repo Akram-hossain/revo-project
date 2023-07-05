065423b49eb28986d6c18edb360c648a074f6566
Akram Hossain <nayanbd17@gmail.com>
2023-07-05 10:43:30 +0600

    services page

---
+++ assets/js/config.js
@@ -0,0 +1,30 @@
+/* all slider start */
+$(document).ready(function () {
+  $('.feedback-carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoPlay: true,
+    centerMode: true,
+    centerPadding: '420px',
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '120px',
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '50px',
+        }
+      }
+    ]
+  });
+
+});
